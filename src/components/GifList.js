ea9d7fa718311569a02d2c31633a970f9e9e083a
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-25 01:13:29 +0200

    Do not show final result if loading is true

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs'
+/* import React, { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs' */
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import GifItem from './GifItem'
 
 const GifList = ({ category }) => {
-  const [images, setImages] = useState([])
+  const { data: images, loading } = useFetchGifs(category)
+  /* const [images, setImages] = useState([])
   useEffect(() => {
     console.log(category)
     getGifs(category).then(gifs => setImages(gifs))
-  }, [category])
+  }, [category]) */
 
   return (
     <>
       <hr />
-      <div className="gifs-grid grid-columns">
-        {images.map(image => (
-          <GifItem key={image.id} item={image} />
-        ))}
-      </div>
+      {loading ? (
+        <p>loading</p>
+      ) : (
+        <div className="gifs-grid grid-columns">
+          {images.map(image => (
+            <GifItem key={image.id} item={image} />
+          ))}
+        </div>
+      )}
     </>
   )
 }
