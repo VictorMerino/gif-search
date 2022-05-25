39096a75262f21d2082fa8fad10e8cb67678acf3
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-25 02:06:44 +0200

    https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@ export const getGifs = async category => {
     const giphyApiKey = process.env.REACT_APP_GIPHY_API_KEY
     const query = encodeURI(category)
     const limit = 12
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${query}&limit=${limit}`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${query}&limit=${limit}`
 
     const resp = await fetch(url)
     const { data } = await resp.json()
