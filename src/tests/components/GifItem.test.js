501c674ec8ea397a8ca29995cabe21c5a91ee579
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-27 20:44:50 +0200

    __

--- src/tests/components/GifItem.test.js
+++ src/tests/components/GifItem.test.js
@@ -6,7 +6,7 @@ const item = {
   url: 'https://media4.giphy.com/media/3o6Mbnll2gudglC3HG/giphy.gif?cid=2cf3b0b63gqcfo8yeddulrdpoihlaj0nzdsq9n2x1ryoftd6&rid=giphy.gif&ct=g',
 }
 describe('GifItem', () => {
-  test('should show item with class', /* async */ () => {
+  test('should show item with alt text', () => {
     render(<GifItem item={item} />)
     expect(screen.getByAltText(item.title).outerHTML).toContain(item.id)
   })
