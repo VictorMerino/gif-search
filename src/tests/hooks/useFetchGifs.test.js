ecf36a482d6ce9837eb2110f57bbddc9c0641a02
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-28 01:22:21 +0200

    Try another test: no luck, it does not work

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,10 +2,26 @@ import { renderHook } from '@testing-library/react'
 import { useFetchGifs } from '../../hooks/useFetchGifs'
 
 describe('useFetchGifs', () => {
-  test('returns initial state', () => {
+  test('returns initial state', async () => {
     // const { data, loading } = useFetchGifs('Makoto')
-    const { result } = renderHook(() => useFetchGifs('Makoto'))
+    const { result /* waitForNextUpdate */ } = renderHook(() =>
+      useFetchGifs('Makoto')
+    )
     const { data, loading } = result.current
+
+    // await waitForNextUpdate()
+    console.log(data, loading)
+    expect(data).toEqual([])
+    expect(loading).toBe(true)
+  })
+
+  test.skip('returns images array and not loading', async () => {
+    // const { data, loading } = useFetchGifs('Makoto')
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs('Makoto')
+    )
+    const { data, loading } = result.current
+    await waitForNextUpdate()
 
     console.log(data, loading)
     expect(data).toEqual([])
