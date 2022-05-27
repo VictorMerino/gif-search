e036767cdf4d326283cc0b4d0fb6b799b25fae43
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-27 22:12:22 +0200

    Better with submit, as we are actually handling a form

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -6,13 +6,12 @@ describe('AddCategory', () => {
   test('should show item with alt text', async () => {
     render(<AddCategory addCategory={addCategory} />)
     const addInput = await screen.findByTestId('add-input')
-    const addButton = await screen.findByTestId('add-button')
-
-    const newInputValue = { target: { value: 'Nobita' } }
-    fireEvent.change(addInput, newInputValue)
-    fireEvent.click(addButton)
 
     const form = await screen.findByRole('form')
+    const newInputValue = { target: { value: 'Nobita' } }
+    fireEvent.change(addInput, newInputValue)
+    fireEvent.submit(form)
+
     console.log(form.outerHTML)
     expect(true).toBe(true)
   })
